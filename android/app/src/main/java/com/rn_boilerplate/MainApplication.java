054c5cb842f0ef2857aeffda9934edf597300035
Sukhdeep Singh <sukhdeepmanku@gmail.com>
2020-11-02 20:38:02 +0530

    added device info

--- android/app/src/main/java/com/rn_boilerplate/MainApplication.java
+++ android/app/src/main/java/com/rn_boilerplate/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.art.ARTPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
